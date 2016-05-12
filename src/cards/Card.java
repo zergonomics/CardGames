ac5f5bb022838c99581b2f9ec55973a27c1e5b6d
Sung-Hoon Kim <sungspot@gmail.com>
2016-05-12 02:52:43 -0400

    docs

--- src/cards/Card.java
+++ src/cards/Card.java
@@ -1,11 +1,18 @@
 package cards;
-
+/**
+ * A Card
+ * @author Sung-Hoon
+ * 
+ */
 public class Card {
-	// lol
 	private String name;
 	public Card (String card) {
 		name = card;
 	}
+	/**
+	 * Get name of card
+	 * @return name of card
+	 */
 	public String getName() {
 		return name;
 	}
--- src/cards/Deck.java
+++ src/cards/Deck.java
@@ -5,9 +5,13 @@ import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 
+/**
+ * 
+ *  Is deck of cards.
+ *  Has no error checking.
+ * @author Sung-Hoon
+ */
 public class Deck {
-	// is deck of cards
-	// has no error checking
 	private LinkedList<String> cards;
 	
 	public Deck() {
@@ -16,21 +20,41 @@ public class Deck {
 	public Deck(List<String> data) {
 		cards = new LinkedList<String> (data);
 	}
+	
+	/**
+	 * Method to add individual cards to the deck. Cards are added to the bottom.
+	 * @param newCards VarArgs, the cards to be added
+	 */
 	public void addIndividualCards(String... newCards) {
 		for(int i = 0; i < newCards.length; i++) {
 			cards.add(newCards[i]);
 		}
 	}
+	/**
+	 * Method to add a {@link List} of cards to the deck. Cards are added to the bottom.
+	 * @param newCards {@link List} of cards to be added
+	 */
 	public void addListOfCards(List<String> newCards) {
 		cards.addAll(newCards);
 	}
+	/**
+	 * Shuffles the deck.
+	 */
 	public void shuffle() {
 		Collections.shuffle(cards);
 	}
-	
+	/**
+	 * Draw a single card from the top of the deck.
+	 * @return a card
+	 */
 	public String draw() {
 		return this.drawMultiple(1).get(0);
 	}
+	/**
+	 * Draw multiple cards from the top of the deck.
+	 * @param numCards The number of cards to draw.
+	 * @return List of cards
+	 */
 	public List<String> drawMultiple(int numCards) {
 		List<String> drawn = new ArrayList<String>();
 		for(int i = 0; i < numCards; i++) {
@@ -38,6 +62,10 @@ public class Deck {
 		}
 		return drawn;
 	}
+	/**
+	 * Get size of deck
+	 * @return size of deck
+	 */
 	public int getSize() {
 		return cards.size();
 	}
